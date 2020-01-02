cb57f73afaef681d243be616a97dd8fdfe2c241c
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2020-01-02 21:34:49 +0100

    impement snippet-highlight

--- snippets/views.py
+++ snippets/views.py
@@ -1,12 +1,22 @@
 from django.contrib.auth.models import User
 from rest_framework import mixins, generics
 from rest_framework import permissions
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework import renderers
 
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
 from snippets.permissions import isOwnerOrReadonly
 
 
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
@@ -23,6 +33,13 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     permissions_classes = [
         permissions.IsAuthenticatedOrReadOnly, isOwnerOrReadonly]
 
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderers_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
