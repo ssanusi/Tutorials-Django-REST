256738df8b7395a19837019e233e2730884afb9d
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2020-01-02 05:29:54 +0100

    refactor using class base view

--- snippets/views.py
+++ snippets/views.py
@@ -43,38 +43,3 @@ class SnippetDetail(APIView):
         snippet = self.get_object(pk)
         snippet.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-
-
-
-
-
-
-
-
-
-
-
-# @csrf_exempt
-# def snippet_detail(request, pk):
-#     try:
-#         snippet = Snippet.objects.get(pk=pk)
-#     except Snippet.DoesNotExit:
-#         return HttpResponse(status=404)
-
-#     if request.method == 'GET':
-#         serializer = SnippetSerializer(snippet)
-#         return JsonResponse(serializer.data)
-
-#     elif request.method == 'PUT':
-#         data = JSONParser().parse(request)
-#         serializer = SnippetSerializer(snippet, data=data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return JsonResponse(serializer.data)
-#         return JsonResponse(serializer.error, status=400)
-
-#     elif request.method == 'DELETE':
-#         snippet.delete()
-#         return HttpResponse(status=204)
